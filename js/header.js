dec230765b365dc28935423de25778bdb535b9c3
JMiller7334 <Jacob-Miller@Hotmail.com>
2025-01-22 03:11:32 -0600

    api project update

--- js/header.js
+++ js/header.js
@@ -1,5 +1,4 @@
 const scrollIndicator = document.querySelector('.scroll-indicator');
-
 const terminalInputElements = document.querySelectorAll('.terminal-textbox, .terminal-send-button, .api-braces');
 
 function toggleTerminalInput() {
@@ -7,38 +6,105 @@ function toggleTerminalInput() {
       if (element.style.display === 'none' || element.style.display === '') {
           element.style.display = 'block';
       } else {
-          element.style.display = 'none';
+          //element.style.display = 'none';
       }
   });
 }
 
-// document.querySelector('#toggleButton').addEventListener('click', toggleTerminalInput);
+let apiStage = 0
+
+const enumCxWriteStage = {
+  getName: 1,
+  getAddress: 2,
+  getPhone: 3,
+  getEmail: 4,
+  getType: 5
+}
+
+let apiGreetingHeader = 'Call api:'
+let apiGreetingBody = 
+`api commands:
+
+-- CUSTOMER TABLE: --
+- write customer
+- read customer
+- read customer id[enter id ie 1]
 
 
-// Typing effect function
-function typeWriter(elementId, text, speed) {
-    let index = 0;
-    const element = document.getElementById(elementId);
-  
-    // Clear existing text
-    element.innerHTML = '';
-  
-    function type() {
-      if (index < text.length) {
-        const currentChar = text.charAt(index);
-        // Check for newline and replace with <br>
-        if (currentChar === '\n') {
-          element.innerHTML += '<br>';
-        } else {
-          element.innerHTML += currentChar;
-        }
-        index++;
-        setTimeout(type, speed);
-      }
-    }
-  
-    type();
+-- USAGE TABLE: --
+- write usage
+- read usage
+- read usage id[enter id ie 1]
+
+
+please enter a command:
+`;
+
+//-- API CALL BUTTON CLICK --
+document.querySelector('.api-call-button').addEventListener('click', () => {
+  if (isTyping === true) {
+    return;
   }
+
+  toggleTerminalInput();
+  window.scrollTo({top: 0, behavior: 'smooth'});
+
+  isTyping = true;
+  typeWriter('greeting', apiGreetingHeader, 50);
+
+  setTimeout(function() {
+      safeTypeWriter('skills', apiGreetingBody, 15);
+  }, apiGreetingHeader.length * 60);
+
+  setTimeout(function() {
+  }, apiGreetingBody.length * 10);
+});
+
+
+
+
+// -- TYPING EFFECT FUNCTIONS --
+let isTyping = false;
+
+function safeTypeWriter(elementId, text, speed) {
+  if (isTyping) return;
+  isTyping = true;
+
+  typeWriter(elementId, text, speed);
+  setTimeout(() => {
+    isTyping = false;
+  }, text.length * speed + 3000);
+}
+
+function typeWriter(elementId, text, speed) {
+  const element = document.getElementById(elementId);
+  if (!element) {
+    console.error(`Element with ID "${elementId}" not found.`);
+    return;
+  }
+
+  element.innerHTML = '';
+  let index = 0;
+
+  function type() {
+    if (index < text.length) {
+      const currentChar = text.charAt(index);
+      if (currentChar === '\n') {
+        element.innerHTML += '<br>';
+      } else {
+        element.innerHTML += currentChar;
+      }
+
+      index++;
+      setTimeout(type, speed);
+
+    } else {
+      isTyping = false;
+    }
+  }
+  type();
+}
+
 
 
 
@@ -62,10 +128,11 @@ Explore my projects and skills below!`;
 
 //type out the greeting first, then skills
 window.onload = function() {
-    typeWriter('greeting', greetingText, 50); //speed is 100ms per letter
+  isTyping = true;
+  typeWriter('greeting', greetingText, 50); //speed is 100ms per letter
 
     setTimeout(function() {
-        typeWriter('skills', skillsText, 15);
+        safeTypeWriter('skills', skillsText, 15);
     }, greetingText.length * 60);
 
     setTimeout(function() {
