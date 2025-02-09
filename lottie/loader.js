c11004864fa4244b2d16bddee6f9d7c36d9a2e4d
JMiller7334 <Jacob-Miller@Hotmail.com>
2025-02-08 22:14:11 -0600

    major update, built background, better CSS

---
+++ lottie/loader.js
@@ -0,0 +1,7 @@
+var gearAnimation = lottie.loadAnimation({
+    container: document.getElementById("lottie-coding"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "./lottie/coding-animation.json"
+});
