dfce1652104c587dbe19de4cbf7527ff03950961
JMiller7334 <Jacob-Miller@Hotmail.com>
2025-02-19 14:07:46 -0600

    debounce for about-me expandable to fix bug

--- js/about.js
+++ js/about.js
@@ -3,15 +3,22 @@
  * all javascript code related to front end operation for that section will
  * appear here.
  */
+const moreAboutSection = document.getElementById("more-about");
+const button = document.getElementById("toggle-more-about");
 
+var aboutDebounce = false;
+const TRANSITION_LENGTH = 500;
 
 // handles expanding and closing the more about expandable section
 document.getElementById("toggle-more-about").addEventListener("click", () => {
-    const moreAboutSection = document.getElementById("more-about");
-    const button = document.getElementById("toggle-more-about");
 
+    if (aboutDebounce) {
+        return;
+    }
+    aboutDebounce = true;
+
+    //collapse section:
     if (moreAboutSection.classList.contains("expanded")) {
-        // Collapse the section
         moreAboutSection.style.maxHeight = moreAboutSection.scrollHeight + "px"; // Set height before transition
         requestAnimationFrame(() => {
             moreAboutSection.style.maxHeight = "0";
@@ -21,6 +28,11 @@ document.getElementById("toggle-more-about").addEventListener("click", () => {
         button.textContent = "More About Me";
         moreAboutSection.classList.remove("expanded");
 
+        setTimeout(() => {
+            aboutDebounce = false;
+        }, TRANSITION_LENGTH);
+
+    //expand section:
     } else {
         // Expand the section
         moreAboutSection.style.maxHeight = moreAboutSection.scrollHeight + "px"; 
@@ -32,7 +44,8 @@ document.getElementById("toggle-more-about").addEventListener("click", () => {
         // Reset maxHeight after animation
         setTimeout(() => {
             moreAboutSection.style.maxHeight = "none";
-        }, 500); // Match CSS transition duration
+            aboutDebounce = false;
+        }, TRANSITION_LENGTH);
     }
 });
 
